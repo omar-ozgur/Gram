6b76ce6c309b3b76efd8f8c8cee96dc7ddc61718
Omar Ozgur <oozgur217@gmail.com>
2017-06-24 13:52:59 -0700

    Added port flag

--- config/server.go
+++ config/server.go
@@ -1,35 +1,33 @@
 package config
 
 import (
-	"github.com/omar-ozgur/gram/utilities"
+	"flag"
 	"os"
 )
-
-var defaultPort = "3000"
 
 type Args struct {
 	Port string
 }
 
 func GetPort() string {
-	var port = os.Getenv("PORT")
+	defaultPort := "3000"
 
-	// Set a default port if there is nothing in the environment
+	port := os.Getenv("GRAM_PORT")
+
+	// Set default port if there is nothing in the environment
 	if port == "" {
 		port = defaultPort
-		utilities.Sugar.Infof("No PORT environment variable detected, defaulting to port %s", port)
 	}
+
 	return port
 }
 
 func ParseArgs() Args {
 	var args Args
 
-	// TODO: Attempt to get port from command line arguments
-	// Else:
-	args.Port = GetPort()
+	flag.StringVar(&args.Port, "port", GetPort(), "Specifies the port for the server to run on. Ex: --port 3000")
 
-	// TODO: Parse other arguments
+	flag.Parse()
 
 	return args
 }
