58acac24439591c32c2f77a1029974555ab6ec22
Omar Ozgur <oozgur217@gmail.com>
2017-06-25 14:56:04 -0700

    Removed unnecessary default parameter

--- utilities/defaults.go
+++ utilities/defaults.go
@@ -3,7 +3,6 @@ package utilities
 const DefaultPort = "3000"
 
 const DefaultDBUser = "root"
-const DefaultDBPassword = "root"
 const DefaultDBName = "gram"
 const DefaultDBHost = "localhost"
 const DefaultDBSSLMode = "disable"
